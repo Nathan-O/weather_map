dfd65af3d1a2a69f5466d08abeff88760d1ade39
Nathan Ockerman <ockerman.ns@gmail.com>
2016-03-14 13:53:38 -0700

    basic sanity checks.

---
+++ server.js
@@ -0,0 +1,38 @@
+// * REQUIREMENTS * //
+var express = require("express");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var path = require("path");
+var methodOverride = require("method-override");
+// var db = require("./models");
+
+var app = express();
+
+
+// * CONFIG * //
+app.use("/static", express.static("public"));
+app.use("/vender", express.static("bower_components"));
+
+app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
+var views = path.join(process.cwd(), "views/");
+
+
+// * ROUTES * //
+
+// Home Route
+app.get("/", function(req, res){
+   res.sendFile(views + "index.html");
+});
+
+// * SERVER * //
+app.listen(3000, function(){
+   console.log("Yes?! What is it?");
+});
+
+// app.listen(process.env.PORT || 3000, function(){
+// 	console.log("We're running wild!");
+// });
