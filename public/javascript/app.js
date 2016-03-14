d25fce0eba416ad99401855b69d925ee5455cf97
Nathan Ockerman <ockerman.ns@gmail.com>
2016-03-14 14:45:45 -0700

    google maps renders.

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
    console.log("Sanity");
 });
+
+/*
+NOTE: Map 'Zoom Level' generals:
+1: World
+5: Landmass/continent
+10: City
+15: Streets
+20: Buildings
+*/
+function initMap() {
+   console.log("In initMap()");
+   map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+   });
+
+   setTimeout(function(){
+      alert("Hello");
+
+   }, 3000);
+}
